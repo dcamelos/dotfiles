b76d9f02b663e39d0d734485a57c370f8ff96413
dcamelos <dcamelos@unal.edu.co>
2026-03-30 11:44:02 -0500

    update polybar and palette.sh

--- wm/.config/polybar/palette.sh
+++ wm/.config/polybar/palette.sh
@@ -6,57 +6,26 @@ desfase_x=2
 desfase_y=1
 separacion=5
 espacio_inicial=10
-marco_color=7
 
 echo -e "\n"
 IFS='[;' read -p $'\e[6n' -d R -rs _ line col
 
 for i in {0..7}; do
-
   pos_x=$((espacio_inicial + (i * (ancho + separacion))))
 
-  # --- MARCO FINO para Color 0 ---
-
-  if [ $i -eq 0 ]; then
-
-    # Color del texto (38) para que la línea sea delgada
-
-    # Esquina superior
-
-    printf "\e[${line};$((pos_x - 1))H\e[38;5;${marco_color}m┌$(printf '─%.0s' $(seq 1 $ancho))┐\e[0m"
-
-    # Laterales
-
-    for ((l = 1; l < alto; l++)); do
-
-      printf "\e[$((line + l));$((pos_x - 1))H\e[38;5;${marco_color}m│\e[$((ancho))C│\e[0m"
-
-    done
-
-    # Esquina inferior
-
-    printf "\e[$((line + alto));$((pos_x - 1))H\e[38;5;${marco_color}m└$(printf '─%.0s' $(seq 1 $ancho))┘\e[0m"
-
-  fi
+  # Si el color es 0, usamos el 237 para que no desaparezca en el fondo negro
+  col_back=$i
+  [[ $i -eq 0 ]] && col_back=237
 
   # --- BARRA ATRÁS ---
-
   for ((l = 0; l < alto; l++)); do
-
-    # Se dibuja encima del marco, dejando ver solo los bordes laterales
-
-    printf "\e[$((line + l));$((pos_x + 1))H\e[48;5;${i}m%*s\e[0m" $ancho ""
-
+    printf "\e[$((line + l));$((pos_x))H\e[48;5;${col_back}m%*s\e[0m" $ancho ""
   done
 
   # --- BARRA ADELANTE ---
-
   for ((l = 0; l < alto; l++)); do
-
-    printf "\e[$((line + l + desfase_y));$((pos_x + 1 + desfase_x))H\e[48;5;$((i + 8))m%*s\e[0m" $ancho ""
-
+    printf "\e[$((line + l + desfase_y));$((pos_x + desfase_x))H\e[48;5;$((i + 8))m%*s\e[0m" $ancho ""
   done
-
 done
 
 printf "\e[$((line + alto + desfase_y + 1));1H\n"
